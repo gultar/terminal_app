ea082493618f741ba1602570faabad10473e74b4
gultar <sacha-olivier.dulac@hotmail.com>
2018-05-03 00:02:58 -0400

    uyfug

---
+++ backend/ipFinder.js
@@ -0,0 +1,28 @@
+var net = require('net');
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+
+  return '0.0.0.0';
+}
+
+module.exports = { getIPAddress };
+
+//function getNetworkIP(callback) {
+//   var socket = net.createConnection(80, 'www.google.com');
+//   socket.on('connect', function() {
+//     callback(undefined, socket.address().address);
+//     socket.end();
+//   });
+//   socket.on('error', function(e) {
+//     callback(e, 'error');
+//   });
+// }
