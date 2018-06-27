d5c576d806b7975e974df7f30292391a097d1c78
gultar <sacha-olivier.dulac@hotmail.com>
2018-06-27 12:10:05 -0400

    deleted all that junk, moved it a directory up

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const http = require('http');
-const app = express();
-const port = 8080
-const server = http.createServer(app);
-app.use(express.static(__dirname+'/views'));
-
-module.exports = { app, server, port }
